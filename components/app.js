a09cd7a6e6a37e57907014a170fc4ee1675962c5
MarioSalinas <mario_1605@live.com.mx>
2021-08-10 10:41:37 -0500

    feat: allow children components

---
+++ components/app.js
@@ -0,0 +1,21 @@
+import { Component } from '../lib/react.js'
+import User from './user.js'
+import Wrapper from './wrapper.js'
+
+class App extends Component {
+    render() {
+        return `
+        <div class="app">
+            ${new Wrapper({
+            children: new User({
+                avatar: './images/ash.jpg',
+                name: 'Ash',
+            }).render()
+        }).render()
+            }
+        </div>
+        `
+    }
+}
+
+export default App
