598bdca763901cba16e3d95edac84352b3049259
MarioSalinas <mario_1605@live.com.mx>
2021-08-18 07:17:09 -0500

    feat: add styled components and replace user section

--- components/app.js
+++ components/app.js
@@ -1,16 +1,23 @@
 import { Component } from '../lib/react.js'
 import User from './user.js'
 import Wrapper from './wrapper.js'
+import UserStyled from './user-styled.js'
 
 class App extends Component {
     render() {
         return `
         <div class="app">
             ${new Wrapper({
-            children: new User({
+            children: `
+                ${new User({
                 avatar: './images/ash.jpg',
                 name: 'Ash',
-            }).render()
+            }).render()}
+                ${new UserStyled({
+                avatar: './images/ash.jpg',
+                name: 'Ash',
+            }).render()}
+            `
         }).render()
             }
         </div>
